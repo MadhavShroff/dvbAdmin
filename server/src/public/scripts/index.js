87ace2000075bfdb480b6a64b4ccecb55f020544
Madhav Shroff <madhavshroff99@gmail.com>
2021-04-17 13:09:19 +0530

    Modified SSL certs

--- server/src/public/scripts/index.js
+++ server/src/public/scripts/index.js
@@ -1,4 +1,7 @@
 import QrScanner from "../assets/qrscanner/qr-scanner.min.js";
+
+console.log(QrScanner);
+
 QrScanner.WORKER_PATH = '../assets/qrscanner/qr-scanner-worker.min.js';
 
 const video = $("#preview")[0]
@@ -11,13 +14,14 @@ QrScanner.hasCamera().then(hasCamera => {
 });
 
 const scanner = new QrScanner(video, result => {
+    console.log("Hello");
     if(result !== "No QR code found") {
         scanner.stop()
         window.location = result;
     }
 }, error => console.log(error));
 
-$(startScanButton).click(() => {
+$(startScanButton).on("click", () => {
     if(isScannerActive) { // Disable Scanner
         video.style.display = 'none';
         scanner.stop();
@@ -25,6 +29,7 @@ $(startScanButton).click(() => {
     } else { // Enable Scanner
         video.style.display = 'block'
         scanner.start().then(() => {
+            console.log("Hello");
             scanner.hasFlash().then(hasFlash => {
                 if (hasFlash) {
                     // TODO: If flash is available, show a button to enable/disable it
